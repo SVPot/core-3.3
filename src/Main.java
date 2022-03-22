8cf1646e862e23d02f36067163ec251c661862f9
Sergey Potehin <spot7200@gmail.com>
2022-03-22 13:57:09 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (openZip("C:/Games/savegames/savegames.zip", "C:/Games/savegames"))
+            System.out.println("Распаковано успешно");
+
+        GameProgress gameProgress = openProgress("C:/Games/savegames/savegame2.dat");
+        System.out.println(gameProgress);
+    }
+
+    public static boolean openZip(String zipPath, String path) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(zipPath))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public static GameProgress openProgress(String path) {
+        GameProgress gameProgress = null;
+        try (FileInputStream fis = new FileInputStream(path);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return gameProgress;
+    }
+}
